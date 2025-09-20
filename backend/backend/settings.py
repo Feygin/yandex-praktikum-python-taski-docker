ed204ae80c916bc2e872b0dd9e9f32f62c4b9493
Alexander Feygin <A.Feygin@alfabank.ru>
2025-09-20 18:24:55 +0300

    fixed formatting in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.23.30', '127.0.0.1', 'localhost','feygin-taski.ddns.net']
+ALLOWED_HOSTS = ['51.250.23.30', '127.0.0.1', 'localhost', 'feygin-taski.ddns.net']
 
 # Application definition
 
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 # в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / 'collected_static'
 # Теперь при вызове команды python manage.py collectstatic
-# Django будет копировать все статические файлы в директорию collected_static 
+# Django будет копировать все статические файлы в директорию collected_static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
